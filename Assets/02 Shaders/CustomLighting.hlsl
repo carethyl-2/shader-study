20205607c1bd141ca4e7c4dc511432839cdf487e
CareBear <CareBear-Nerd@users.noreply.github.com>
2025-10-24 12:52:40 +1300

    Started adding custom lighting shader

---
+++ Assets/02 Shaders/CustomLighting.hlsl
@@ -0,0 +1,106 @@
+#ifndef CUSTOM_LIGHTING_INCLUDED
+#define CUSTOM_LIGHTING_INCLUDED
+
+
+#pragma multi_compile _ _MAIN_LIGHT_SHADOWS
+#pragma multi_compile _ _MAIN_LIGHT_SHADOWS_CASCADE
+#pragma multi_compile _ _SHADOWS_SOFT _SHADOWS_SOFT_LOW _SHADOWS_SOFT_MEDIUM _SHADOWS_SOFT_HIGH
+#pragma multi_compile _ _ADDITIONAL_LIGHTS_VERTEX _ADDITIONAL_LIGHTS
+#pragma multi_compile _ _ADDITIONAL_LIGHT_SHADOWS
+#pragma multi_compile _ _CLUSTER_LIGHT_LOOP
+
+#ifndef SHADERGRAPH_PREVIEW
+    #include "Packages/com.unity.render-pipelines.universal/Editor/ShaderGraph/Includes/ShaderPass.hlsl"
+    #if (SHADERPASS != SHADERPASS_FORWARD)
+        #undef REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR
+    #endif
+#endif
+
+struct CustomLightingData
+{
+    // Position and Orientation
+    float3 positionWorldSpace;
+    float3 normalWorldSpace;
+    float3 viewDirectionWorldSpace;
+    float4 shadowCoordinate;
+
+    // Surface properties
+    float3 albedo;
+    float smoothness;
+};
+
+float GetSmoothnessPower(float _rawSmoothness)
+{
+    return exp2(10 * _rawSmoothness + 1);
+}
+
+
+#ifndef SHADERGRAPH_PREVIEW
+float3 CustomLightHandling(CustomLightingData _data, Light _light)
+{
+    // Light base color
+    float3 radiance = _light.color * _light.shadowAttenuation;
+
+    // Diffuse
+    float diffuse = saturate(dot(_data.normalWorldSpace, _light.direction));
+
+    // Specular
+    float specularDot = saturate(dot(_data.normalWorldSpace, normalize(_light.direction + _data.viewDirectionWorldSpace)));
+    float specular = pow(specularDot, GetSmoothnessPower(_data.smoothness))* diffuse;
+
+
+    // Combined light outputs
+    float3 color = _data.albedo * radiance * (diffuse + specular);
+    return color;
+}
+#endif
+
+float3 CustomLighting(CustomLightingData _data)
+{
+    // Shader graph preview estimate
+#ifdef SHADERGRAPH_PREVIEW
+    float3 lightDirection = float3(0.5, 0.5, 0);
+    float intensity = saturate(dot(_data.normalWorldSpace, lightDirection)) + 
+        pow(saturate(dot(_data.normalWorldSpace, normalize(_data.viewDirectionWorldSpace + lightDirection))), GetSmoothnessPower(_data.smoothness));
+    return _data.albedo * intensity;
+#else
+
+    // Core functionality
+
+    // Get main light
+    Light mainLight = GetMainLight(_data.shadowCoordinate, _data.positionWorldSpace, 1);
+    float3 color = 0;
+
+    // Shade the main light
+    color += CustomLightHandling(_data, mainLight);
+    return color;
+#endif
+}
+
+void CustomLighting_float(float3 PositionWS, float3 NormalWS, float3 ViewDirWS, float3 Albedo, float Smoothness, out float3 Color)
+{
+    CustomLightingData data;
+
+    data.positionWorldSpace = PositionWS;
+    data.normalWorldSpace = NormalWS;
+    data.viewDirectionWorldSpace = ViewDirWS;
+    data.albedo = Albedo;
+    data.smoothness = Smoothness;
+
+// Preview
+#ifdef SHADERGRAPH_PREVIEW
+    data.shadowCoordinate = 0;
+
+// Not preview
+#else
+    float4 positionCS = TransformWorldToHClip(PositionWS);
+    #if SHADOWS_SCREEN
+        data.shadowCoordinate = ComputeScreenPos(positionCS);
+    #else
+        data.shadowCoordinate = TransformWorldToShadowCoord(PositionWS);
+    #endif
+#endif
+
+    Color = CustomLighting(data);
+}
+#endif // CUSTOM_LIGHTING_INCLUDED
